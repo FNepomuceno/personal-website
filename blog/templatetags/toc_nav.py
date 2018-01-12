6986d301a61d406ce45bd9820658517af5d7e3ab
Francis Nepomuceno <francisnepo@gmail.com>
2018-01-12 22:49:07 +0000

    Version 0 Update 9 - Moved Table of Contents to Sidebar
    - Navigation bar now contains table of contents of blog post
    - Added indications of nested items in navigation bar
    - Sticky removed for compatibility with smaller screens

--- blog/templatetags/toc_nav.py
+++ blog/templatetags/toc_nav.py
@@ -51,9 +51,11 @@ class ContentExtractor(Treeprocessor):
             self.append_section(section_stack, new_level, old_level,
                 list_item)
         elif old_level == 0:
-            list_item = etree.Element("ul")
+            list_top = etree.Element("ul")
+            list_item = etree.Element("li")
             list_item.append(self.make_link("Top", section.get("id")))
-            section_stack[0] = list_item
+            list_top.append(list_item)
+            section_stack[0] = list_top
         else:
             list_item = etree.Element("li")
             list_item.append(self.make_link(section[0].text,
@@ -80,3 +82,70 @@ class MakeToc(Extension):
 def mark_toc(text):
     return to_markdown(text, extensions=[EscapeHtml(), TagsPlus(),
         MakeToc()], output_format='html5')
+
+class NavBarMaker(Treeprocessor):
+    def run(self, root):
+        contents = ContentExtractor().run(root)
+        new_tree = self.convert_tree(contents[0],
+            "#article-top", "Contents")
+        new_root = etree.Element("div")
+        new_root.append(new_tree)
+        return new_root
+
+    def convert_tree(self, root, link, title):
+        new_tree = self.make_nesting(link, title)
+        for item in list(root):
+            if len(list(item)) > 1:
+                nesting = self.convert_tree(item[1],
+                    item[0].get("href"), item[0].text)
+                new_tree[1][0].append(nesting)
+
+                new_link = etree.Element("a")
+                new_link.set("href", item[0].get("href"))
+                new_link.text = "Main"
+                nesting[1][0].insert(0, self.make_link(new_link))
+            else:
+                new_tree[1][0].append(self.make_link(item[0]))
+        return new_tree
+
+    def make_link(self, link):
+        list_item = etree.Element("li")
+        link.set("class", "nav-link sidenav-item")
+        list_item.append(link)
+        return list_item
+
+    def make_nesting(self, link, title):
+        list_item = etree.Element("li")
+        list_item.set("class", "nav-item dropdown")
+
+        raw_link = search("#article-(.*)", link).group(1)
+        new_link = "toc-{}".format(raw_link)
+
+        link_item = etree.Element("a")
+        link_item.set("class", "nav-link sidenav-item")
+        link_item.set("data-toggle", "collapse")
+        link_item.set("data-target", "{}{}".format("#", new_link))
+        link_item.set("href", "javascript:void(0);")
+        link_item.set("aria-expanded", "false")
+        link_item.set("aria-controls", new_link)
+        link_item.text = "{} {}".format(title, "▼")
+        list_item.append(link_item)
+
+        div_item = etree.Element("div")
+        div_item.set("class", "collapse")
+        div_item.set("id", new_link)
+        list_item.append(div_item)
+
+        ul_item = etree.Element("ul")
+        ul_item.set("class", "nav flex-column")
+        div_item.append(ul_item)
+        return list_item
+
+class MakeTocNav(Extension):
+    def extendMarkdown(self, md, md_globals):
+        md.treeprocessors.add('tocizer', NavBarMaker(md), '_end')
+
+@register.filter
+def mark_nav(text):
+    return to_markdown(text, extensions=[EscapeHtml(), TagsPlus(),
+        MakeTocNav()], output_format='html5')
