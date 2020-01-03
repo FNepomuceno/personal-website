38e70a2e6b3730296dcd3a238ac77d6cb41dae0e
FNepomuceno <francisnepo@gmail.com>
2020-01-03 20:13:58 +0000

    Updated Blog list view to show posts

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
 def index(request):
-    return render(request, 'blog/list.html')
+    return post_list(request)
 
+def post_list(request):
+    posts = Post.objects.all()
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/list.html', context)
